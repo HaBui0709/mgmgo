634ef7ccb3bf0b2f6d7bfc848ce7f46be3d7b8b9
Nghia Tran <Nghia.Tran@mgm-tp.com>
2019-07-22 11:07:04 +0000

    feature/EXP-91-find-current-address-via-geocoding-api
    Squashed commit of the following:
    
    commit bd3f9b98c78029be44b4edafbae4f8d6f235b624
    Author: dhnguyen.ext <Neverfall0126>
    Date:   Mon Jul 22 16:38:47 2019 +0700
    
        EXP-91: update UI
    
    commit 42f1936e453355ecf79a1e788e5bdd25ee3a897b
    Author: dhnguyen.ext <Neverfall0126>
    Date:   Mon Jul 22 15:51:45 2019 +0700
    
        EXP-91: fix conflict
    
    commit 207e9d8a008492635c8e7a53af8d1af797782899
    Author: Nghia Tran <Nghia.Tran@mgm-tp.com>
    Date:   Mon Jul 22 08:31:13 2019 +0000
    
        feature/EXP-140-find-current-address-via-geododing-api
    
        Squashed commit of the following:
    
        commit e43d5426509cbe7033557381a3563471d46e79cd
        Author: dhnguyen.ext <Neverfall0126>
        Date:   Mon Jul 22 15:18:12 2019 +0700
    
            EXP-140: rewrite code for find location
    
        commit e69ce171011107d9d3e8cf6f49868f1702700cac
        Author: dhnguyen.ext <Neverfall0126>
        Date:   Mon Jul 22 13:33:32 2019 +0700
    
            EXP-140: fix unit test for service not working after refactor
    
        commit e9779f5a6f2de26d13845d1034e23a0b74eca0ea
        Author: dhnguyen.ext <Neverfall0126>
        Date:   Mon Jul 22 11:18:08 2019 +0700
    
            EXP-140: move url reverse service to application.properties
    
        commit d937ead77e30906840282915470c7497519ebd15
        Author: dhnguyen.ext <Neverfall0126>
        Date:   Fri Jul 19 17:17:36 2019 +0700
    
            EXP-140 write code for finding address by geocoding api
    
            add unit test for service and controller
    
        commit 8db8e4966dca5e41242a1c85972388956f3a39a8
        Author: dhnguyen.ext <Neverfall0126>
        Date:   Wed Jul 17 17:52:54 2019 +0700
    
            EXP-140: write code for find current address via geocoding api

--- src/main/webapp/static/js/activity.js
+++ src/main/webapp/static/js/activity.js
@@ -67,3 +67,76 @@ $(document).ready(function () {
         }
     })
 })
+
+function setMessageWhenFailed(msg) {
+    $("#spinner").removeClass("spinner-border").removeClass("text-success");
+    $("#gps-return").removeClass("alert-danger").removeClass("alert-success").removeClass("d-none");
+    $("#gps-return").addClass("alert-danger");
+    $("#gps-return").text(msg != null ? msg : "Something went wrong! Please try again.");
+    $("#addressInputLabel").text("");
+}
+
+function hideMessage() {
+    $("#gps-return").addClass("d-none");
+}
+
+$("#findLocation").click(function () {
+    hideMessage();
+    $("#confirmLocationBtn").attr("disabled", "disabled");
+    $("#addressInput").val("");
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            $("#spinner").addClass("spinner-border").addClass("text-success");
+            $("#addressInputLabel").text("Getting your location, please wait...");
+            let urlRequest = '/location/activity/' + position.coords.latitude.toString() + '/' + position.coords.longitude + '/';
+            $.get(
+                urlRequest
+            ).done(function (data) {
+                if (data.addressGeocoding) {
+                    hideMessage();
+                    $("#addressInput").val(data.addressGeocoding);
+                    $("#spinner").removeClass("spinner-border").removeClass("text-success");
+                    $("#addressInputLabel").text("");
+                    $("#confirmLocationBtn").removeAttr('disabled');
+                } else {
+                    setMessageWhenFailed("Something went wrong! Please try again.");
+                }
+            }).fail(function (data) {
+                setMessageWhenFailed(data.message);
+            });
+        }, showErrorWhileGetLocation);
+    } else {
+        setMessageWhenFailed("Geolocation is not supported by this browser.");
+    }
+})
+
+function showErrorWhileGetLocation(error) {
+    $("#spinner").removeClass("spinner-border").removeClass("text-success");
+    switch (error.code) {
+        case error.PERMISSION_DENIED:
+            setMessageWhenFailed("User denied the request for Geolocation, check your internet connection and try again!");
+            break;
+        case error.POSITION_UNAVAILABLE:
+            setMessageWhenFailed("Location information is unavailable, check your browser and try again!");
+            break;
+        case error.TIMEOUT:
+            setMessageWhenFailed("The request to get user location timed out, check your internet connection and try again!");
+            break;
+        case error.UNKNOWN_ERROR:
+            setMessageWhenFailed("Something went wrong! Please try again.");
+            break;
+    }
+}
+
+$("#confirmLocationBtn").click(function () {
+    $("#address").val($("#addressInput").val().replace(/\s+/g, ' ').trim());
+    $("#modalGetLocation").modal('hide');
+})
+
+$("#address").focus(function () {
+    $("#findLocation").css('border-top-color', '#80bdff').css('border-right-color', '#80bdff').css('border-bottom-color', '#80bdff');
+})
+
+$("#address").focusout(function () {
+    $("#findLocation").css('border-top-color', '#ced4da').css('border-right-color', '#ced4da').css('border-bottom-color', '#ced4da');
+})
